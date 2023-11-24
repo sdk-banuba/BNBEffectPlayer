fe2ab7f64bbf04a92d47180e155f1f69da4afbc4
ci <ci@banuba.com>
2023-11-24 17:07:11 +0300

    1.9.1-86-g65c1c833d4

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let bnbPackageVersion: Version = "1.9.1-86-g65c1c833d4"
+
+let package = Package(
+    name: "BNBEffectPlayer",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "BNBEffectPlayer",
+            targets: [
+                "BNBEffectPlayer"
+            ]
+        ),
+    ],
+    dependencies: [
+
+    ],
+    targets: [
+        .binaryTarget(
+            name: "BNBEffectPlayer",
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.9.1-86-g65c1c833d4/BNBEffectPlayer.zip",
+            checksum: "9565e8a59a1c984c4ca860e38c897ad13625179474c5835995b406f3805e4c26"
+        ),
+    ]
+)
