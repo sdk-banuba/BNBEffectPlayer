e9d89be901a98f51badf25c28b9f9fb157e4d132
ci <ci@banuba.com>
2024-06-25 11:45:51 +0300

    1.14.1

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.14.0-9-g9cd2ba648"
+let bnbPackageVersion: Version = "1.14.1"
 
 let package = Package(
     name: "BNBEffectPlayer",
@@ -22,8 +22,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BNBEffectPlayer",
-            url: "https://d2cm7wtcqqw29.cloudfront.net/1.14.0-9-g9cd2ba648/BNBEffectPlayer.zip",
-            checksum: "da962049f3db4678c249b3344b90a4276c5eb32c9fffbc3a3c74e65aec1d72cb"
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.14.1/BNBEffectPlayer.zip",
+            checksum: "0834fccaa09b5d76f6e01a64bcbaa595c2e8e33b7ea020a6ee6a58527a2cdd8b"
         ),
     ]
 )
