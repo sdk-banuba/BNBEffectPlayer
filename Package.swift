5bae6c2a9f2e9492811f066eb5d76ea2eb35304f
ci <ci@banuba.com>
2024-12-18 12:50:39 +0300

    1.16.3

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         .binaryTarget(
             name: "BNBEffectPlayer",
             url: "https://d2cm7wtcqqw29.cloudfront.net/1.16.3/BNBEffectPlayer.zip",
-            checksum: "7f950b4ed42002743e4b8104d769154276d4e219424c66c6594ce1a0ceb75f46"
+            checksum: "9cb238b86ac24a64e8aac9c5ae31c7f29e734f135a813c486f4e8a97969b7f74"
         ),
     ]
 )
